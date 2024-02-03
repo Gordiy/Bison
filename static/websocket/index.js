324c3a6ba9b2db76c7aefc59a00e35d97f4f8cdd
hrushyne <hordii-yurii.rushynets@pe.lifecell.com.ua>
2024-02-03 19:46:51 +0200

    Update frontend script

--- static/websocket/index.js
+++ static/websocket/index.js
@@ -1,17 +1,32 @@
 const chatSocket = new WebSocket('ws://' + window.location.host + '/ws/balance/');
 
-chatSocket.onmessage = function(e) {
+chatSocket.onmessage = function (e) {
     const data = JSON.parse(e.data);
     const message = data['message'];
-    console.log('message')
+    console.log("Message:", message)
+
+    // Handle incoming message
+    const chatMessages = document.getElementById('chat-messages');
+    const messageElement = document.createElement('p');
+    messageElement.textContent = message;
+    chatMessages.appendChild(messageElement);
 };
 
-chatSocket.onclose = function(e) {
+chatSocket.onclose = function (e) {
     console.error('Chat socket closed unexpectedly');
 };
 
-function sendMessage(message) {
-    chatSocket.send(JSON.stringify({
-        'message': message
-    }));
+// Send message to server
+function sendMessage() {
+    const messageInput = document.getElementById('message-input');
+    const message = messageInput.value.trim();
+
+    if (message) {
+        chatSocket.send(JSON.stringify({
+            'message': message
+        }));
+
+        // Clear the input field
+        messageInput.value = '';
+    }
 }
