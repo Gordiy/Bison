f670a223aa974eba77adc246a1b349d75b1db1f4
hrushyne <hordii-yurii.rushynets@pe.lifecell.com.ua>
2024-02-03 19:48:08 +0200

    run app using uvicorn

--- balance/consumer.py
+++ balance/consumer.py
@@ -17,5 +17,5 @@ class BalanceConsumer(WebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         self.send(text_data=json.dumps({
-            'message': message
+            'message': f'backend message: {message}'
         }))
