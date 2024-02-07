fa010998d5c49c75a834574cf7a97686851aff2b
hrushyne <hordii-yurii.rushynets@pe.lifecell.com.ua>
2024-02-07 14:35:53 +0000

    add balance property

--- balance/services.py
+++ balance/services.py
@@ -5,11 +5,48 @@ import requests
 
 from balance.constants import (MERCHANT_ACCOUNT, MERCHANT_SIGNATURE,
                                WAYFORPAY_BASE_URL)
+from django.core.cache import cache
 
 
 class WayForPayService:
     """Service to use WayForPay API."""
-    def get_balace(self) -> int or None:
+    balance_cache_key = 'balance'
+
+    @property
+    def balance(self):
+        """
+        Get the current balance from the cache, or fetch it from the API if not cached.
+
+        :return: The current balance in UAH.
+        """
+        cached_balance = cache.get(self.balance_cache_key)
+
+        if cached_balance is None:
+            balance = self.get_balance()
+            cache.set(self.balance_cache_key, balance)
+
+            return balance
+
+        return cached_balance
+    
+    def update_balance(self):
+        """
+        Update the cached balance by fetching the latest balance from the API.
+
+        :return: The updated balance in UAH.
+        """
+        balance = self.get_balance()
+
+        cache.set(self.balance_cache_key, balance)
+
+        return balance
+
+    def get_balance(self) -> int or None:
+        """
+        Get the current balance from the WayForPay API.
+
+        :return: The current balance in UAH, or None if unable to fetch.
+        """
         payload = json.dumps({
             "merchantAccount": MERCHANT_ACCOUNT,
             "merchantSignature": MERCHANT_SIGNATURE
@@ -20,7 +57,6 @@ class WayForPayService:
         response = requests.post(url, data=payload)
 
         if not response.status_code == 200:
-            return 
+            return None
 
         return response.json().get('balance_UAH')
-
